16036d26eb5a273d612a16733e45e2b7559bf057
deveeni <mandarino.viva.21@gmail.com>
2021-06-20 15:46:28 +0900

    add setting files

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const person = {
+    name: "deveein",
+    age: 28,
+    gender: "female"
+};
+const sayHi = (person) => {
+    return `Hello ${person.name}, you are ${person.age}, you ar a ${person.gender}`;
+};
+console.log(sayHi(person));
+//# sourceMappingURL=index.js.map
